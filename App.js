6ad177bfbb44ad54d22ead45b0ef1017283647b4
Randula Wickramasuriya <randularsw@gmail.com>
2020-07-20 16:52:47 +0530

    navigation setup

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from "react";
+import firebase from "./firebase";
+import {
+  View,
+  Text,
+  YellowBox,
+  ImageBackground,
+  StyleSheet,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import AddWorkplace from "./screens/AddWorkplace";
+
+const Stack = createStackNavigator();
+
+function App() {
+  YellowBox.ignoreWarnings(["Setting a timer"]);
+  // firebase.auth().onAuthStateChanged((user) => {
+  //   console.log(user ? user.providerData : "No User App");
+  // });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Add Workplace" component={AddWorkplace} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    // flex: 1,
+    width: "100%",
+    height: "100%",
+    // resizeMode: "cover",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
